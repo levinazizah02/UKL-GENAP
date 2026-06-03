e13c585834c144070c3467ca2b4240cc834022cb
levina_azizah <levina_azizah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-03 12:15:27 +0700

    fix: set CORS origin to localhost for development

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3001',
     credentials: true,
   });
 
