031e46a15601f19bcf62627a38914df0999e09b3
levina_azizah <levina_azizah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-03 12:23:23 +0700

    feat: add AppController and AppService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { UserModule } from './user/user.module';
 import { CustomerModule } from './customer/customer.module';
 import { MenuModule } from './menu/menu.module';
 import { TransaksiModule } from './transaksi/transaksi.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
+  controllers: [AppController],
   imports: [
     PrismaModule,
     AuthModule,
@@ -15,5 +18,6 @@ import { TransaksiModule } from './transaksi/transaksi.module';
     MenuModule,
     TransaksiModule,
   ],
+  providers: [AppService],
 })
 export class AppModule {}
