5cde8313cfc06e882b590bc4c2a795346d6ff4a3
levina_azizah <levina_azizah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-04 08:38:25 +0700

    fixing typo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
-@Controller('au th')
+@Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
